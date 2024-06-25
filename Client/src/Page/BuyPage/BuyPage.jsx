7c13cfe1db6bbf644e9fdb1a0a505c127dcdbc39
Alismayil <ali.ismayil.681@gmail.com>
2024-06-25 19:58:11 +0400

    new

--- Client/src/Page/BuyPage/BuyPage.jsx
+++ Client/src/Page/BuyPage/BuyPage.jsx
@@ -12,18 +12,27 @@ function BuyPage() {
                     <p>These glasses detect objects using Object Recognition Technology. Then, through Audio Feedback technology, they convey what they see as sound to visually impaired individuals. Additionally, these glasses stand out with a Text Reading function. This function allows detecting words in different languages, besides the native language, which certainly facilitates user input to information. Charging of the glasses is carried out via rechargeable batteries. One of its superior features compared to its competitors is its special Azerbaijani Friendly Software provision.</p>
                 </div>
                 <div className="contactBox">
-                    <div className="iconBox">
-                        <p><FaInstagram /></p>
-                        <h5><a href="">echovision2024</a></h5>
-                    </div>
-                    <div className="iconBox">
-                        <p><MdOutlinePhone /></p>
-                        <h5><a href="tel:077 336 95 55">+994 77 336 95 55</a></h5>
-                    </div>
-                    <div className="iconBox">
-                        <p><CiMail /></p>
-                        <h5 style={{ fontSize: "15px" }}><a href='mailto:echovisioncompany@gmail.com'>echovisioncompany@gmail.com</a></h5>
-                    </div>
+                    <a href="https://www.instagram.com/echovision2024/">
+                        <div className="iconBox">
+                            <p><FaInstagram /></p>
+                            <h5>echovision2024</h5>
+                        </div>
+                    </a>
+
+                    <a href="tel:077 336 95 55">
+                        <div className="iconBox">
+                            <p><MdOutlinePhone /></p>
+                            <h5>+994 77 336 95 55</h5>
+                        </div>
+                    </a>
+
+                    <a href='mailto:echovisioncompany@gmail.com'>
+                        <div className="iconBox">
+                            <p><CiMail /></p>
+                            <h5 style={{ fontSize: "10px" }}>echovisioncompany@gmail.com</h5>
+                        </div>
+                    </a>
+
                 </div>
                 <div className="mapBox">
                     <div className="mapIcon"><RiMapPin2Fill /></div>
